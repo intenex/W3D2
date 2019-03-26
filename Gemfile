f90bab4e1640d750af1f5f23fbb25db221582346
Ben Yu <intenex@gmail.com>
2019-03-26 11:33:32 -0700

    Yay

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+gem 'sqlite3'
+gem 'faker', :git => 'https://github.com/stympy/faker.git', :branch => 'master'
---
+++ aaquestions/import_db.sql
@@ -0,0 +1,60 @@
+PRAGMA foreign_keys = ON;
+
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY,
+  fname TEXT NOT NULL,
+  lname TEXT NOT NULL
+);
+
+CREATE TABLE questions (
+  id INTEGER PRIMARY KEY,
+  title VARCHAR(255) NOT NULL,
+  body TEXT NOT NULL,
+  user_id INTEGER NOT NULL,
+
+  FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE question_follows (
+  id INTEGER PRIMARY KEY,
+  user_id INTEGER NOT NULL,
+  question_id INTEGER NOT NULL,
+  
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (question_id) REFERENCES questions(id)
+);
+
+CREATE TABLE replies (
+  id INTEGER PRIMARY KEY,
+  body TEXT NOT NULL,
+  question_id INTEGER NOT NULL,
+  user_id INTEGER NOT NULL,
+  parent_id INTEGER NOT NULL,
+
+  FOREIGN KEY (question_id) REFERENCES questions(id),
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (parent_id) REFERENCES replies(id)
+);
+
+CREATE TABLE question_likes (
+  id INTEGER PRIMARY KEY,
+  user_id INTEGER NOT NULL,
+  question_id INTEGER NOT NULL,
+
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (question_id) REFERENCES questions(id)
+);
+
+INSERT INTO
+  users (fname, lname)
+VALUES
+  ('Aubrie', 'Rice'),
+  ('Ben', 'Yu');
+
+INSERT INTO 
+  questions (title, body, user_id)
+VALUES
+  ('Help what is the question???', 'I don''t know what the question is also what is flex', (SELECT id FROM users WHERE fname = 'Ben' AND lname = 'Yu'));
+
